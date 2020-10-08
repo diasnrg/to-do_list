21ce98f309aad1051d7939753a846acf3ab88c66
vdios <190103485@stu.sdu.edu.kz>
2020-10-08 21:21:45 +0600

    buttons work

--- script.js
+++ script.js
@@ -3,7 +3,18 @@ function submit_task(){
     const task = inputobj.value;
     inputobj.value = '';
     console.log(task);
+    const card = document.createElement('div');
+    card.innerHTML = task;
+    const container = document.querySelector('.container')
+    container.appendChild(card);
 }
+
+function clearContainer(){
+    document.querySelector('.container').innerHTML = '';
+}
+
 const submit = document.querySelector('.bt_submit');
 submit.addEventListener('click',submit_task);
 
+const clear = document.querySelector('.bt_clear');
+clear.addEventListener('click',clearContainer);
